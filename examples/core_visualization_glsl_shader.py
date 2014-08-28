5491189955dd96b6c35dcc0df3de2e037651824a
Jelle Feringa <jelleferinga@gmail.com>
2014-08-28 10:09:39 +0200

    document what's going on in this example
    wip
    
    zzzz
    
    zzzz
    
    zzzzzzzzzzzzzz
    
    wip
    
    wip

--- examples/core_visualization_glsl_shader.py
+++ examples/core_visualization_glsl_shader.py
@@ -15,45 +15,70 @@
 ##You should have received a copy of the GNU Lesser General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
-#-------------------------------------------------------------------------------
+# -------------------------------------------------------------------------------
 # Example for attaching GLSL shaders to shapes
-#-------------------------------------------------------------------------------
+# GLSL stands for OpenGL Shader Language
+# Find out more about GLSL here: http://www.lighthouse3d.com/opengl/glsl/
+# -------------------------------------------------------------------------------
 
+import glob
 import os
-from OCC.Display.SimpleGui import init_display
+import sys
+import time
+
+from OCC.AIS import AIS_Shape
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeSphere
-from OCC.Graphic3d import Graphic3d_ShaderProgram, Graphic3d_TOS_VERTEX, Graphic3d_TOS_FRAGMENT, Graphic3d_ShaderObject
+from OCC.Display.SimpleGui import init_display
+from OCC.Graphic3d import (Graphic3d_ShaderProgram, Graphic3d_TOS_VERTEX, Graphic3d_TOS_FRAGMENT,
+                           Graphic3d_ShaderObject)
+from OCC.IFSelect import IFSelect_RetDone, IFSelect_ItemsByEntity
+from OCC.STEPControl import STEPControl_Reader
 from OCC.TCollection import TCollection_AsciiString
 
 display, start_display, add_menu, add_function_to_menu = init_display()
-my_box = BRepPrimAPI_MakeSphere(20.).Shape()
 
-anIO = display.DisplayShape(my_box, update=True)
-_phong_vs = "/Users/jelleferinga/miniconda/share/oce-0.16/src/Shaders/PhongShading.vs"
-_phong_fs = "/Users/jelleferinga/miniconda/share/oce-0.16/src/Shaders/PhongShading.fs"
+shape = BRepPrimAPI_MakeSphere(100).Shape()
 
-assert os.path.isfile(_phong_fs) and os.path.isfile(_phong_vs), "missing shader programs: {0} or {1}".format(_phong_fs, _phong_vs)
+anIO = AIS_Shape(shape)
+display.Context.Display(anIO.GetHandle())
 
-phong_fs = TCollection_AsciiString(_phong_fs)
-phong_vs = TCollection_AsciiString(_phong_vs)
+shader_dir = "/Users/jelleferinga/GIT/oce/src/Shaders"
 
+# look for the shaders stored in the shader folder...
+# only a single vertex (vs) and fragment (fs) shader for the moment
+_phong_vs = glob.glob(os.path.join(shader_dir, "*.vs"))[0]  # PhongShading.vs
+_phong_fs = glob.glob(os.path.join(shader_dir, "*.fs"))[0]  # PhongShading.fs
+
+# construct TCollection_AsciiString from string
+# fs = TCollection_AsciiString(_phong_fs)
+# vs = TCollection_AsciiString(_phong_vs)
+fs = TCollection_AsciiString("/Users/jelleferinga/GIT/pythonocc-core/examples/shaders/phong.fs.glsl")
+vs = TCollection_AsciiString("/Users/jelleferinga/GIT/pythonocc-core/examples/shaders/phong.vs.glsl")
+# fs = TCollection_AsciiString("/Users/jelleferinga/miniconda/envs/_test/share/oce-0.18-dev/src/Shaders/RaytraceSmooth.fs")
+# vs=TCollection_AsciiString("/Users/jelleferinga/miniconda/envs/_test/share/oce-0.18-dev/src/Shaders/RaytraceRender.vs")
+
+# construct the shader, load, compile and attach the GLSL programs
 aProgram = Graphic3d_ShaderProgram()
-aProgram.AttachShader(Graphic3d_ShaderObject.CreateFromFile(Graphic3d_TOS_FRAGMENT, phong_fs ) )
-aProgram.AttachShader(Graphic3d_ShaderObject.CreateFromFile(Graphic3d_TOS_VERTEX, phong_vs ) )
+fs_shader = Graphic3d_ShaderObject.CreateFromFile(Graphic3d_TOS_FRAGMENT, fs)
+aProgram.AttachShader(fs_shader)
+aProgram.AttachShader(Graphic3d_ShaderObject.CreateFromFile(Graphic3d_TOS_VERTEX, vs))
 
-aspect = anIO.GetObject().Attributes().GetObject().ShadingAspect().GetObject().aspect().GetObject()
-h_aProgram = aspect.ShaderProgram() # returns a Graphic3d_ShaderProgram_Handle
+# >>> aProgram.Variables()
+# Out[2]: <Swig Object of type 'NCollection_Sequence< Handle_Graphic3d_ShaderVariable > *' at 0x1158bc330>
+# >>> aProgram.Variables().GetObject()
 
-#-------------------------------------------------------------------------------
-# h_aProgram -> Graphic3d_ShaderProgram_Handle cannot be cast to a Graphic3d_ShaderProgram
-# aProgram has not method .GetHandle...
-# aspect.SetShader requires a `Graphic3d_ShaderProgram_Handle` instance
-#-------------------------------------------------------------------------------
 
-aspect.SetShaderProgram(aspect.ShaderProgram())
 
-display.Context.Redisplay(anIO)
+# attach the shader to the AIS_Shape representation that renders the sphere
+aspect = anIO.Attributes().GetObject().ShadingAspect().GetObject().aspect().GetObject()
+# aspect.SetShaderProgram(aspect.ShaderProgram())
+
 # type(aspect.ShaderProgram())
 # Out[76]: SwigPyObject
 
+
+display.Context.Redisplay(anIO.GetHandle())
+display.FitAll()
+
+print "ref count ", aProgram.GetRefCount()
 start_display()
