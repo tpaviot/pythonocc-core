bb736426235437004edae901bd3216108540414e
tpaviot <tpaviot@gmail.com>
2014-09-22 17:16:48 +0200

    Added webgl unittest

---
+++ test/core_webgl_unittest.py
@@ -0,0 +1,74 @@
+#!/usr/bin/python
+
+##Copyright 2010-2014 Thomas Paviot (tpaviot@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU Lesser General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU Lesser General Public License for more details.
+##
+##You should have received a copy of the GNU Lesser General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
+from __future__ import print_function
+
+import unittest
+import os
+
+from OCC.Display.WebGl import threejs_renderer
+from OCC.BRepPrimAPI import BRepPrimAPI_MakeTorus, BRepPrimAPI_MakeBox
+
+
+class TestWebGL(unittest.TestCase):
+    def test_render_torus(self):
+        '''Test: point from curve'''
+        torus_shp = BRepPrimAPI_MakeTorus(20., 10.).Shape()
+        my_renderer = threejs_renderer.ThreejsRenderer(background_color="#123345")
+        my_renderer.DisplayShape(torus_shp)
+        assert os.path.isfile('./shape.js')
+        assert os.path.isfile('./webgl_topods_shape.html')
+
+    def test_shaders(self):
+        '''Test: point from curve'''
+        torus_shp = BRepPrimAPI_MakeBox(20., 10., 30.).Shape()
+        vertex_shader = """
+            attribute vec3 center;
+            varying vec3 vCenter;
+            void main() {
+                vCenter = center;
+                gl_Position = projectionMatrix * modelViewMatrix * vec4( position, 1.0 );
+            }
+        """
+        fragment_shader = """
+            #extension GL_OES_standard_derivatives : enable
+            varying vec3 vCenter;
+            float edgeFactorTri() {
+                vec3 d = fwidth( vCenter.xyz );
+                vec3 a3 = smoothstep( vec3( 0.0 ), d * 1.5, vCenter.xyz );
+                return min( min( a3.x, a3.y ), a3.z );
+            }
+            void main() {
+                gl_FragColor.rgb = mix( vec3( 1.0 ), vec3( 0.2 ), edgeFactorTri() );
+                gl_FragColor.a = 1.0;
+            }
+            """
+        my_renderer = threejs_renderer.ThreejsRenderer("#123345", vertex_shader, fragment_shader)
+        my_renderer.DisplayShape(torus_shp)
+        assert os.path.isfile('./shape.js')
+        assert os.path.isfile('./webgl_topods_shape.html')
+
+
+def suite():
+    suite = unittest.TestSuite()
+    suite.addTest(unittest.makeSuite(TestWebGL))
+    return suite
+
+if __name__ == "__main__":
+    unittest.main()
