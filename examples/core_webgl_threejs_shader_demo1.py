103e9f5fd67d3f46b0c6f7081c382d5d747b69d7
tpaviot <tpaviot@gmail.com>
2014-09-23 12:21:44 +0200

    Fixed shader demo

--- examples/core_webgl_threejs_shader_demo1.py
+++ examples/core_webgl_threejs_shader_demo1.py
@@ -21,10 +21,10 @@ from OCC.Display.WebGl import threejs_renderer
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeTorus
 
 # create box
-cube_shp = BRepPrimAPI_MakeTorus(30., 20.).Shape()
+cube_shp = BRepPrimAPI_MakeTorus(25., 15.).Shape()
 # load shaders
-vs = open('shaders/fresnel.vs', 'r').read()
-fs = open('shaders/fresnel.fs', 'r').read()
-u = open('shaders/fresnel.uniforms', 'r').read()
+vs = open('shaders/pink.vs', 'r').read()
+fs = open('shaders/pink.fs', 'r').read()
+u = open('shaders/pink.uniforms', 'r').read()
 my_renderer = threejs_renderer.ThreejsRenderer(vertex_shader=vs, fragment_shader=fs, uniforms = u)
 my_renderer.DisplayShape(cube_shp)
