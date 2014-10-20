51cea2880625c61d40139b51ae707b5b50acbfbd
tpaviot <tpaviot@gmail.com>
2014-10-20 22:13:06 +0200

    PEP8 improvements; removed menu item for atomic examples ; misc fixes/improvements

--- examples/core_animation.py
+++ examples/core_animation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 
-##Copyright 2009-2013 Thomas Paviot (tpaviot@gmail.com)
+##Copyright 2009-2014 Thomas Paviot (tpaviot@gmail.com)
 ##
 ##This file is part of pythonOCC.
 ##
@@ -20,12 +20,12 @@
 from __future__ import print_function
 
 import time
+from math import pi
 
-from OCC.gp import *
+from OCC.gp import gp_Ax1, gp_Pnt, gp_Dir, gp_Trsf
 from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox
-from OCC.TopLoc import *
-from OCC.AIS import *
-from OCC.Display.SimpleGui import *
+from OCC.TopLoc import TopLoc_Location
+from OCC.Display.SimpleGui import init_display
 
 display, start_display, add_menu, add_function_to_menu = init_display()
 
@@ -39,6 +39,7 @@ def build_shape():
 
 
 def rotating_cube_1_axis(event=None):
+    display.EraseAll()
     ais_boxshp = build_shape()
     ax1 = gp_Ax1(gp_Pnt(0., 0., 0.), gp_Dir(0., 0., 1.))
     aCubeTrsf = gp_Trsf()
@@ -50,27 +51,27 @@ def rotating_cube_1_axis(event=None):
         aCubeToploc = TopLoc_Location(aCubeTrsf)
         display.Context.SetLocation(ais_boxshp, aCubeToploc)
         display.Context.UpdateCurrentViewer()
-        angle += 0.1
+        angle += 2*pi / n_rotations
     print("%i rotations took %f" % (n_rotations, time.time() - tA))
 
 
 def rotating_cube_2_axis(event=None):
+    display.EraseAll()
     ais_boxshp = build_shape()
     ax1 = gp_Ax1(gp_Pnt(0., 0., 0.), gp_Dir(0., 0., 1.))
     ax2 = gp_Ax1(gp_Pnt(0., 0., 0.), gp_Dir(0., 1., 0.))
-    aCubeTrsf = gp_Trsf()
-    aCubeTrsf2 = gp_Trsf()
+    a_cube_trsf = gp_Trsf()
+    a_cube_trsf2 = gp_Trsf()
     angle = 0.0
-    angle2 = 0.0
     tA = time.time()
     n_rotations = 200
     for i in range(n_rotations):
-        aCubeTrsf.SetRotation(ax1, angle)
-        aCubeTrsf2.SetRotation(ax2, angle)
-        aCubeToploc = TopLoc_Location(aCubeTrsf*aCubeTrsf2)
+        a_cube_trsf.SetRotation(ax1, angle)
+        a_cube_trsf2.SetRotation(ax2, angle)
+        aCubeToploc = TopLoc_Location(a_cube_trsf * a_cube_trsf2)
         display.Context.SetLocation(ais_boxshp, aCubeToploc)
         display.Context.UpdateCurrentViewer()
-        angle += 0.1
+        angle += 2*pi / n_rotations
     print("%i rotations took %f" % (n_rotations, time.time() - tA))
 
 if __name__ == '__main__':
