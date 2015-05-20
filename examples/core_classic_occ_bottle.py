93075a4b54263a4ff3245ec358411d6b2776ffdf
Jeremy Wright <wrightjmf@gmail.com>
2015-05-20 16:44:05 -0400

    Added Copyright and License Header to Example

--- examples/core_classic_occ_bottle.py
+++ examples/core_classic_occ_bottle.py
@@ -1,5 +1,22 @@
 #!/usr/bin/env python
 
+##Copyright 2009-2015 Thomas Paviot (tpaviot@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU Lesser General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU Lesser General Public License for more details.
+##
+##You should have received a copy of the GNU Lesser General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
 import math
 
 from OCC.gp import gp_Pnt, gp_OX, gp_Vec, gp_Trsf, gp_DZ, gp_Ax2, gp_Ax3, gp_Pnt2d, gp_Dir2d, gp_Ax2d
