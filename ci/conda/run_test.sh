178ec877a486b62d176590d0dddc78d07df50bdd
tpaviot <tpaviot@gmail.com>
2016-01-23 07:02:14 +0100

    add graph tests to conda

--- ci/conda/run_test.sh
+++ ci/conda/run_test.sh
@@ -1,3 +1,9 @@
-export MMGT_OPT=2
 cd $SRC_DIR/test
 python run_tests.py
+python core_webgl_unittest.py
+if [ `uname` == Linux ]; then
+    xvfb-run -s "-screen 0 1024x768x16" python core_display_pyqt4_unittest.py
+    xvfb-run -s "-screen 0 1024x768x16" python core_display_pyqt5_unittest.py
+    xvfb-run -s "-screen 0 1024x768x16" python core_display_pyside_unittest.py
+    xvfb-run -s "-screen 0 1024x768x16" python core_display_wx_unittest.py
+fi
