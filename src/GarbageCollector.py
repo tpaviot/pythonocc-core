6ab1d7aac8560ec9d4ca8423a2a3de5b395fac1c
tpaviot <tpaviot@gmail.com>
2016-02-09 19:09:35 +0100

    Defused GarbageCollector

--- src/GarbageCollector.py
+++ src/GarbageCollector.py
@@ -15,86 +15,38 @@
 ##You should have received a copy of the GNU Lesser General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
-# Init logging system
-from collections import deque
+import warnings
+warnings.warn("Deprecation Warning: do not use the GarbageCollector anymore, it is unneeded.")
 
 
 class GarbageCollector(object):
-    """ Garbage collector for OCC objects
+    """ DEPRECATED. Garbage collector for OCC objects
     """
     def __init__(self):
-        #self._collected_objects = []
-        self._handles = deque()
-        self._transients = deque()
-        self._misc = deque()
-
-        # The list for the prevented objects
-        self._prevent = deque()
-
-        # define contexts lists
-        self._handles_contexts = deque()
-        self._transients_contexts = deque
-        self._misc_contexts = deque()
+        pass
 
     def prevent_object(self, obj):
-        ''' Prevent an object from being deleted. This object is temporary
+        ''' DEPRECATED. Prevent an object from being deleted. This object is temporary
         stored in a list to increase its refecount.
         '''
-        self._prevent.append(obj)
+        pass
 
     def push_context(self):
-        ''' Create a new context. When the push context method is called,
+        ''' DEPRECATED. Create a new context. When the push context method is called,
         objects are collected in this new context. When the context is popped,
         erase the content of the context.
         '''
-        # store the current objects in the contexts list
-        self._handles_contexts.append(self._handles)
-        self._transients_contexts.append(self._transients)
-        self._misc_contexts.append(self._misc)
-        # Then erase the content of the current lists
-        self._handles = deque()
-        self._transients = deque()
-        self._misc = deque()
+        pass
 
     def pop_context(self):
-        self._handles = self._handles_contexts.pop()
-        self._transients = self._transients_contexts.pop()
-        self._misc = self._misc_contexts.pop()
+        pass
 
     def collect_object(self, obj_deleted):
-        ''' This method is called whenever a pythonOCC instance is deleted.'''
-        if hasattr(obj_deleted, 'was_purged'):
-            return False
-        if obj_deleted.__class__.__name__.startswith('Handle'):
-            self._handles.append(obj_deleted)
-        elif hasattr(obj_deleted, "GetHandle"):
-            self._transients.append(obj_deleted)
-        else:
-            self._misc.append(obj_deleted)
+        ''' DEPRECATED. This method is called whenever a pythonOCC instance is deleted.'''
+        pass
 
     def smart_purge(self):
-        # TODO: need to reverse the lists first in order
-        # to start with the olders objects?
-        # Remove Handles
-        while len(self._handles) > 0:
-            handle = self._handles.pop()
-            handle.Nullify()  # decrement ref counting_kill_pointed()
-            # after that, the reference counting of transients objects
-            # in the garbage should be 0
-            handle.was_purged = True
-        # Remove Transients
-        for transient in list(self._transients):
-            transient_must_be_purged = False
-            if transient.GetRefCount() == 0:
-                transient_must_be_purged = True
-            if transient_must_be_purged:
-                #transient._kill_pointed()
-                transient.was_purged = True
-                self._transients.remove(transient)
-        # Remove other objects
-        while len(self._misc) > 0:
-            misc = self._misc.pop()
-            misc._kill_pointed()
-            misc.was_purged = True
+        # DEPRECATED.
+        pass
 
 garbage = GarbageCollector()
