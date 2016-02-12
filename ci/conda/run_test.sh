4d26ce2b844b4513e0c75aee41c126fb6de0be86
tpaviot <tpaviot@gmail.com>
2016-02-12 16:58:03 +0100

    run an xvfb server before running the GUI tests on Linux/Travis

--- ci/conda/run_test.sh
+++ ci/conda/run_test.sh
@@ -2,8 +2,13 @@ cd $SRC_DIR/test
 python run_tests.py
 python core_webgl_unittest.py
 if [ `uname` == Linux ]; then
-    xvfb-run -s "-screen 0 1024x768x16" python core_display_pyqt4_unittest.py
-    xvfb-run -s "-screen 0 1024x768x16" python core_display_pyqt5_unittest.py
-    xvfb-run -s "-screen 0 1024x768x16" python core_display_pyside_unittest.py
-    xvfb-run -s "-screen 0 1024x768x16" python core_display_wx_unittest.py
+	# start xvfb
+	export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    sleep 3 # give xvfb some time to start
+    # then run GUI tests
+    python core_display_pyqt4_unittest.py
+    python core_display_pyqt5_unittest.py
+    python core_display_pyside_unittest.py
+    python core_display_wx_unittest.py
 fi
