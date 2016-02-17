e4bae89a975400ee63001260f274677c4a7d31ba
tpaviot <tpaviot@gmail.com>
2016-02-17 07:38:35 +0100

    Try to figure out gui test, set up pixel depth an screen size in xvfb

--- ci/conda/run_test.sh
+++ ci/conda/run_test.sh
@@ -4,8 +4,9 @@ python run_examples_as_tests.py
 python core_webgl_unittest.py
 if [ `uname` == Linux ]; then
 	# start xvfb
-	export DISPLAY=:99.0
-    sh -e /etc/init.d/xvfb start
+	#export DISPLAY=:99.0
+    #sh -e /etc/init.d/xvfb start
+    /sbin/start-stop-daemon --start --pidfile /tmp/custom_xvfb_99.pid --make-pidfile --background --exec /usr/bin/Xvfb -- :99 -ac -screen 0 1024x768x16
     sleep 3 # give xvfb some time to start
     # then run GUI tests
     python core_display_pyqt4_unittest.py
