af7943dae5e0c413c6d69c8ce7a828467a1330aa
tpaviot <tpaviot@gmail.com>
2016-04-27 09:20:13 +0200

    Modified load_ap203 example such as the read_step_file function can be used from other examples

--- examples/core_load_step_ap203.py
+++ examples/core_load_step_ap203.py
@@ -23,20 +23,25 @@ from OCC.STEPControl import STEPControl_Reader
 from OCC.IFSelect import IFSelect_RetDone, IFSelect_ItemsByEntity
 from OCC.Display.SimpleGui import init_display
 
-step_reader = STEPControl_Reader()
-status = step_reader.ReadFile('./models/as1_pe_203.stp')
+def read_step_file(filename):
+    step_reader = STEPControl_Reader()
+    status = step_reader.ReadFile(filename)
 
-if status == IFSelect_RetDone:  # check status
-    failsonly = False
-    step_reader.PrintCheckLoad(failsonly, IFSelect_ItemsByEntity)
-    step_reader.PrintCheckTransfer(failsonly, IFSelect_ItemsByEntity)
+    if status == IFSelect_RetDone:  # check status
+        failsonly = False
+        step_reader.PrintCheckLoad(failsonly, IFSelect_ItemsByEntity)
+        step_reader.PrintCheckTransfer(failsonly, IFSelect_ItemsByEntity)
 
-    ok = step_reader.TransferRoot(1)
-    _nbs = step_reader.NbShapes()
-    aResShape = step_reader.Shape(1)
-else:
-    print("Error: can't read file.")
-    sys.exit(0)
-display, start_display, add_menu, add_function_to_menu = init_display()
-display.DisplayShape(aResShape, update=True)
-start_display()
+        ok = step_reader.TransferRoot(1)
+        _nbs = step_reader.NbShapes()
+        aResShape = step_reader.Shape(1)
+    else:
+        print("Error: can't read file.")
+        sys.exit(0)
+    return aResShape
+
+if __name__ == "__main__":
+    the_shape = read_step_file('./models/as1_pe_203.stp')
+    display, start_display, add_menu, add_function_to_menu = init_display()
+    display.DisplayShape(the_shape, update=True)
+    start_display()
