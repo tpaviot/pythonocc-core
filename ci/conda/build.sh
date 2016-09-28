b585d6b0d028174ae5c0dd19b46eaa454897b639
Martin Siggel <martin.siggel@dlr.de>
2016-09-28 23:31:08 +0200

    Install swig modules properly
    By installing the modules instead of copying them,
    cmake can do its magic and set the rpaths correctly.

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -33,11 +33,9 @@ mkdir -p $PREFIX/src
 mkdir -p $PREFIX/src/pythonocc-core
 cp -r src $PREFIX/src/pythonocc-core
 
-# fix python and freetype loader path in all modules
+# fix rpaths
 if [ `uname` == Darwin ]; then
     for lib in `ls $SP_DIR/OCC/_*.so`; do
-      install_name_tool -change $PY_LIB @rpath/$PY_LIB $lib
-      install_name_tool -change libfreetype.6.dylib @rpath/libfreetype.6.dylib $lib
       install_name_tool -rpath $PREFIX/lib @loader_path/../../../ $lib
     done
 fi
