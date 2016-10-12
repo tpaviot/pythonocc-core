083b4518edb2f3bf981bdb53e21d9e038dced729
tpaviot <tpaviot@gmail.com>
2016-10-12 08:38:30 +0200

    fixes to algotopia basic example port

--- examples/core_modeling_bool_demo.py
+++ examples/core_modeling_bool_demo.py
@@ -23,27 +23,25 @@
 from math import atan, cos, sin, pi
 
 from OCC.BRepAlgoAPI import BRepAlgoAPI_Cut, BRepAlgoAPI_Fuse
-from OCC.BRepBuilderAPI import BRepBuilderAPI_Transform, BRepBuilderAPI_MakeWire, BRepBuilderAPI_MakeEdge, \
-    BRepBuilderAPI_MakeFace
+from OCC.BRepBuilderAPI import (BRepBuilderAPI_Transform, BRepBuilderAPI_MakeWire,
+                                BRepBuilderAPI_MakeEdge, BRepBuilderAPI_MakeFace)
 from OCC.BRepFeat import BRepFeat_MakeCylindricalHole
-from OCC.BRepPrimAPI import BRepPrimAPI_MakeSphere, BRepPrimAPI_MakeCylinder, BRepPrimAPI_MakeTorus, \
-    BRepPrimAPI_MakeRevol
-from OCC.Display.SimpleGui import init_display
+from OCC.BRepPrimAPI import (BRepPrimAPI_MakeSphere, BRepPrimAPI_MakeCylinder,
+                             BRepPrimAPI_MakeTorus, BRepPrimAPI_MakeRevol)
 from OCC.TColgp import TColgp_Array1OfPnt
 from OCC.gp import gp_Ax2, gp_Pnt, gp_Dir, gp_Ax1, gp_Trsf, gp_Vec
 
-display, start_display, _, __, = init_display()
+from OCC.Display.SimpleGui import init_display
+display, start_display, add_menu, add_function_to_menu = init_display()
 
-M_PI = pi / 2.0
 
 def generate_shape():
     """Create a sphere with faces top and bottom"""
     sphere_radius = 1.0
     sphere_angle = atan(0.5)
-
     sphere_origin = gp_Ax2(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1))
-    sphere = BRepPrimAPI_MakeSphere(sphere_origin, sphere_radius, -sphere_angle, sphere_angle).Shape()
-
+    sphere = BRepPrimAPI_MakeSphere(sphere_origin, sphere_radius,
+                                    -sphere_angle, sphere_angle).Shape()
     return sphere
 
 
@@ -51,12 +49,10 @@ def add_feature(base):
     """Add a "feature" to a shape. In this case we drill a hole through it."""
     feature_diameter = 0.8
     feature_origin = gp_Ax1(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1))
-
     feature_maker = BRepFeat_MakeCylindricalHole()
     feature_maker.Init(base, feature_origin)
     feature_maker.Build()
     feature_maker.Perform(feature_diameter / 2.0)
-    ## requires BOPCol to query result???
     shape = feature_maker.Shape()
     return shape
 
@@ -65,22 +61,19 @@ def boolean_cut(base):
     # Create a cylinder
     cylinder_radius = 0.25
     cylinder_height = 2.0
-    cylinder_origin = (gp_Pnt(0.0, 0.0, - cylinder_height / 2.0), gp_Dir(0.0, 0.0, 1.0))
+    cylinder_origin = gp_Ax2(gp_Pnt(0.0, 0.0, - cylinder_height / 2.0), gp_Dir(0.0, 0.0, 1.0))
     cylinder = BRepPrimAPI_MakeCylinder(cylinder_origin, cylinder_radius, cylinder_height)
 
     # Repeatedly move and subtract it from the input shape
     move = gp_Trsf()
-    boolean_result = base;
-    clone_radius = 1.0;
+    boolean_result = base
+    clone_radius = 1.0
 
-    # for (int clone = 0; clone <= 7; ++clone)
-    for clone in range(7):
-        angle = clone * M_PI / 4.0
-
+    for clone in range(8):
+        angle = clone *  pi / 4.0
         # Move the cylinder
         move.SetTranslation(gp_Vec(cos(angle) * clone_radius, sin(angle) * clone_radius, 0.0))
         moved_cylinder = BRepBuilderAPI_Transform(cylinder.Shape(), move, True).Shape()
-
         # Subtract the moved cylinder from the drilled sphere
         boolean_result = BRepAlgoAPI_Cut(boolean_result, moved_cylinder).Shape()
     return boolean_result
@@ -89,8 +82,8 @@ def boolean_cut(base):
 def boolean_fuse(base):
     ring_radius = 0.25
     torus_radius = 1.0 - ring_radius
-    torus = BRepPrimAPI_MakeTorus(torus_radius, ring_radius)
-    fuse = BRepAlgoAPI_Fuse(base, torus.Shape())
+    torus = BRepPrimAPI_MakeTorus(torus_radius, ring_radius).Shape()
+    fuse = BRepAlgoAPI_Fuse(base, torus).Shape()
     return fuse
 
 
@@ -113,18 +106,14 @@ def revolved_cut(base):
         face_points.SetValue(n + 1, i)
 
     # Use these points to create edges and add these edges to a wire
-    # BRepBuilderAPI_MakeWire hexwire;
-
-    # for (Standard_Integer i = 1; i < 7; i++)
-
     hexwire = BRepBuilderAPI_MakeWire()
 
     for i in range(1, 7):
-        hexedge = BRepBuilderAPI_MakeEdge(face_points[i], face_points[i + 1])
+        hexedge = BRepBuilderAPI_MakeEdge(face_points.Value(i), face_points.Value(i + 1)).Edge()
         hexwire.Add(hexedge)
 
     # Turn the wire into a 6 sided face
-    hexface = BRepBuilderAPI_MakeFace(hexwire.Wire())
+    hexface = BRepBuilderAPI_MakeFace(hexwire.Wire()).Face()
 
     # Revolve the face around an axis
     revolve_axis = gp_Ax1(gp_Pnt(0, 0, 0), gp_Dir(0, 0, 1))
@@ -133,7 +122,7 @@ def revolved_cut(base):
     # Move the generated shape
     move = gp_Trsf()
     move.SetTranslation(gp_Pnt(0, 0, 0), gp_Pnt(0, 0, sin(0.5)))
-    moved_shape = BRepBuilderAPI_Transform(revolved_shape, move, False)
+    moved_shape = BRepBuilderAPI_Transform(revolved_shape, move, False).Shape()
 
     # Remove the revolved shape
     cut = BRepAlgoAPI_Cut(base, moved_shape).Shape()
@@ -152,4 +141,5 @@ def generate_demo():
 if __name__ == "__main__":
     demo = generate_demo()
     display.DisplayShape(demo)
+    display.FitAll()
     start_display()
