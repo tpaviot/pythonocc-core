bad3df522939688060ce3c159acb8ee028f9f194
Thomas Paviot <tpaviot@gmail.com>
2017-05-25 10:09:55 +0200

    Rollback retina fix, earlier fix is not longer required see #426

--- src/Display/qtDisplay.py
+++ src/Display/qtDisplay.py
@@ -99,29 +99,6 @@ class qtBaseViewer(QtOpenGL.QGLWidget):
             self._display.OnResize()
 
 
-def handle_retina(_nsview_pointer):
-    """ OSX only. hack to detect and handle Retina display
-    """
-    screen = QtGui.QGuiApplication.instance().primaryScreen()
-    pixelDensity = screen.physicalDotsPerInch()
-
-    if pixelDensity >= 147:
-        log.info("Retina display detected")
-        try:
-            import objc
-        except ImportError:
-            log.critical("pyobjc is not installed... try pip installing it \npip install pyobjc")
-        else:
-            nsview = objc.objc_object(c_void_p=_nsview_pointer)
-            nsview.setWantsBestResolutionOpenGLSurface_(True)
-            scaleFactor = nsview.backingScaleFactor()
-            rec1 = screen.geometry()
-            nsview.setBounds_(((rec1.x(),
-                                rec1.y()),
-                               (rec1.width() * scaleFactor,
-                                rec1.height() * scaleFactor)))
-
-
 class qtViewer3d(qtBaseViewer):
 
     # emit signal when selection is changed
@@ -158,9 +135,6 @@ class qtViewer3d(qtBaseViewer):
         self._qApp = value
 
     def InitDriver(self):
-        if sys.platform == "darwin":
-            handle_retina(self.GetHandle())
-
         self._display = OCCViewer.Viewer3d(self.GetHandle())
         self._display.Create()
         # background gradient
