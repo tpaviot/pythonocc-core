d7fbe98e0651a3bf2667072cadae07db880e11eb
Thomas Paviot <tpaviot@gmail.com>
2017-12-21 16:55:42 +0100

    fixes to jupyter renderer

--- src/Display/WebGl/jupyter_renderer.py
+++ src/Display/WebGl/jupyter_renderer.py
@@ -23,26 +23,55 @@ import tempfile
 
 try:
     from pythreejs import *
-    from IPython.display import display
+    from IPython.display import display, HTML
     from ipywidgets import HTML, Text
     from traitlets import link, dlink
     import numpy as np
 except ImportError:
     raise AssertionError("You must install pythreejs and dependencies to benefit from the jupyter renderer")
 
+from OCC.Bnd import Bnd_Box
+from OCC.BRepBndLib import brepbndlib_Add
 from OCC.Visualization import Tesselator
 
-default_shape_material = PhongMaterial(color='#a6a6a6', shininess=0.9, morphTargets=True)
+# default values
+default_shape_color = '#%02x%02x%02x' % (166, 166, 166)
+
+
+def get_boundingbox(shape, tol=1e-5):
+    """ return the bounding box of the TopoDS_Shape `shape`
+    Parameters
+    ----------
+    shape : TopoDS_Shape or a subclass such as TopoDS_Face
+        the shape to compute the bounding box from
+    tol: float
+        tolerance of the computed boundingbox
+    use_mesh : bool
+        a flag that tells whether or not the shape has first to be meshed before the bbox
+        computation. This produces more accurate results
+    """
+    bbox = Bnd_Box()
+    bbox.SetGap(tol)
+    brepbndlib_Add(shape, bbox, False)
+
+    x_min, y_min, z_min, x_max, y_max, z_max = bbox.Get()
+    x_size = x_max - x_min
+    y_size = y_max - y_min
+    z_size = z_max - z_min
+    x_center = x_min + x_size / 2
+    y_center = y_min + y_size / 2
+    z_center = z_min + z_size / 2
+    return x_size, y_size, z_size, x_center, y_center, z_center
+
 
 class JupyterRenderer(object):
-    def __init__(self):
-        self._camera = PerspectiveCamera(position=[30, 30, 30],
-                                         fov=20,
-                                         children=[DirectionalLight(color='#ffffff', position=[50, 50, 50], intensity=0.5)])
-        self._background = 'black'
+    def __init__(self, size='100%%'):
+        self._background = 'white'
         self._background_opacity = 1
-        
-    def DisplayShape(self, shp, shp_material = default_shape_material):
+        self._size = size
+
+
+    def DisplayShape(self, shp, shape_color = default_shape_color):
         """ takes a topods_shape and displays it in a jupyter notebook
         """
         # compute the tesselation
@@ -71,15 +100,25 @@ class JupyterRenderer(object):
 
         # finally DisplayShape
         shape_geometry = PlainBufferGeometry(vertices=np_vertices, faces=np_faces)
+        #shape_geometry.exec_three_obj_method('computeFaceNormals') TODO normals should be client-side computed
+        #shape_geometry.exec_three_obj_method('computeVertexNormals') TODO idem for vertex normals
+        shp_material = PhongMaterial(color=default_shape_color, shininess=0.9, morphTargets=True)
         shape_mesh = Mesh(geometry=shape_geometry, material = shp_material)
-        scene_shp = Scene(children=[shape_mesh, AmbientLight(color='#ffffff')])
+        scene_shp = Scene(children=[shape_mesh, AmbientLight(color='#101010')])
+
+        # create a camera
+        px, py, pz, xc, yc, zc = get_boundingbox(shp)
+        self._camera = PerspectiveCamera(position=[0, -3*py, pz *3],
+                                         fov=50,
+                                         children=[DirectionalLight(color='#ffffff', position=[50, 50, 50], intensity=0.5)])
         renderer_shp = Renderer(camera=self._camera,
                                 background=self._background,
                                 background_opacity=self._background_opacity,
                                 scene = scene_shp,
-                                controls=[OrbitControls(controlling=self._camera)])
-
+                                controls=[TrackballControls(controlling=self._camera)])
         display(renderer_shp)
+
+
 
 if __name__ == "__main__":
     from OCC.BRepPrimAPI import BRepPrimAPI_MakeBox, BRepPrimAPI_MakeTorus
