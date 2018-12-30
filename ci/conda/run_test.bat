a66c18526021e13ab0d5ce842ecdb3e776cf766a
Thomas Paviot <tpaviot@gmail.com>
2018-12-30 16:51:24 +0100

    Removed the demos folder and run example as tests - takes too long on travis to complete the build

--- ci/conda/run_test.bat
+++ ci/conda/run_test.bat
@@ -1,8 +1,3 @@
 cd ..\work\test
 python run_tests.py
-python run_examples_as_tests.py
 python core_webgl_unittest.py
-python core_display_pyqt4_unittest.py
-python core_display_pyqt5_unittest.py
-python core_display_pyside_unittest.py
-python core_display_wx_unittest.py
