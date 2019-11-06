01312bd90d424b15c257929e22034dfb0621e22c
Thomas Paviot <tpaviot@gmail.com>
2019-11-06 10:41:59 +0100

    update cmake, travis, conda

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -1,38 +1,32 @@
 # make an in source build do to some problems with install
 
-if [ "$PY3K" == "1" ]; then
-    MY_PY_VER="${PY_VER}m"
-else
-    MY_PY_VER="${PY_VER}"
-fi
-
-if [ $(uname) == Darwin ]; then
-    PY_LIB="libpython${MY_PY_VER}.dylib"
-else
-    PY_LIB="libpython${MY_PY_VER}.so"
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*linux.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
 fi
 
 # Configure step
 cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$PREFIX \
- -DPYTHONOCC_BUILD_TYPE=MinSizeRel \
+ -DPYTHONOCC_BUILD_TYPE=Release \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
  -DCMAKE_PREFIX_PATH=$PREFIX \
  -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
- -DPYTHON_EXECUTABLE:FILEPATH=$PYTHON \
- -DPYTHON_INCLUDE_DIR:PATH=$PREFIX/include/python$MY_PY_VER \
- -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/${PY_LIB} \
- -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/${PY_LIB} \
  -DPYTHONOCC_WRAP_SMESH=ON \
  -DSMESH_INCLUDE_PATH:PATH=$PREFIX/include/smesh \
  -DSMESH_LIB_PATH:PATH=$PREFIX/lib \
+ -DPython3_FIND_STRATEGY=LOCATION \
+ -DPython3_FIND_FRAMEWORK=NEVER \
  .
+
 # Build step
-# on linux travis, limit the number of concurrent jobs otherwise
-# gcc gets out of memory
-ninja -j 6 install
+ninja
+
+# Install step
+ninja install
 
 # fix rpaths
-#if [ `uname` == Darwin ]; then
-#    for lib in `ls $SP_DIR/OCC/_*.so`; do
-#      install_name_tool -rpath $PREFIX/lib @loader_path/../../../ $lib
-#    done
-#fi
+if [ `uname` == Darwin ]; then
+    for lib in `ls $SP_DIR/OCC/_*.so`; do
+      install_name_tool -rpath $PREFIX/lib @loader_path/../../../ $lib
+    done
+fi
