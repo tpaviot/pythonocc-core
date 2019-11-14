f57ff6a300d24b1acc3c58f2d3c8426773f8866a
Martin Siggel <martin.siggel@dlr.de>
2019-11-14 16:34:24 +0100

    Fixed python finding in conda

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -1,26 +1,12 @@
 # make an in source build do to some problems with install
 
-if [ "$PY3K" == "1" ]; then
-    MY_PY_VER="${PY_VER}m"
-else
-    MY_PY_VER="${PY_VER}"
-fi
-
-if [ $(uname) == Darwin ]; then
-    PY_LIB="libpython${MY_PY_VER}.dylib"
-else
-    PY_LIB="libpython${MY_PY_VER}.so"
-fi
-
 # Configure step
 cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DPYTHONOCC_BUILD_TYPE=Release \
  -DCMAKE_PREFIX_PATH=$PREFIX \
  -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
- -DPYTHON_EXECUTABLE:FILEPATH=$PYTHON \
- -DPYTHON_INCLUDE_DIR:PATH=$PREFIX/include/python$MY_PY_VER \
- -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/${PY_LIB} \
- -DPYTHON_LIBRARY:FILEPATH=$PREFIX/lib/${PY_LIB} \
+ -DPython3_FIND_STRATEGY=LOCATION \
+ -DPython3_FIND_FRAMEWORK=NEVER \
  -DPYTHONOCC_WRAP_SMESH=OFF \
  -DSMESH_INCLUDE_PATH:PATH=$PREFIX/include/smesh \
  -DSMESH_LIB_PATH:PATH=$PREFIX/lib \
