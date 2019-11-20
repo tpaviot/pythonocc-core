a77e046859096baba71755cb680f996ed5c19743
Thomas Paviot <tpaviot@gmail.com>
2019-11-20 09:53:57 +0100

    Use dlr-sc conda recipe - #711

--- ci/conda/build.sh
+++ ci/conda/build.sh
@@ -1,13 +1,18 @@
 # make an in source build do to some problems with install
 
+declare -a CMAKE_PLATFORM_FLAGS
+if [[ ${HOST} =~ .*linux.* ]]; then
+    CMAKE_PLATFORM_FLAGS+=(-DCMAKE_TOOLCHAIN_FILE="${RECIPE_DIR}/cross-linux.cmake")
+fi
+
 # Configure step
 cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$PREFIX \
  -DPYTHONOCC_BUILD_TYPE=Release \
+  ${CMAKE_PLATFORM_FLAGS[@]} \
  -DCMAKE_PREFIX_PATH=$PREFIX \
  -DCMAKE_SYSTEM_PREFIX_PATH=$PREFIX \
  -DPython3_FIND_STRATEGY=LOCATION \
  -DPython3_FIND_FRAMEWORK=NEVER \
- -DPYTHONOCC_WRAP_SMESH=OFF \
  -DSWIG_HIDE_WARNINGS=ON \
  .
 # Build step
@@ -16,8 +21,8 @@ cmake -G Ninja -DCMAKE_INSTALL_PREFIX=$PREFIX \
 ninja -j 6 install
 
 # fix rpaths
-#if [ `uname` == Darwin ]; then
-#    for lib in `ls $SP_DIR/OCC/_*.so`; do
-#      install_name_tool -rpath $PREFIX/lib @loader_path/../../../ $lib
-#    done
-#fi
+if [ `uname` == Darwin ]; then
+    for lib in `ls $SP_DIR/OCC/_*.so`; do
+      install_name_tool -rpath $PREFIX/lib @loader_path/../../../ $lib
+    done
+fi
