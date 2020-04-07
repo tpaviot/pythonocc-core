50addeb49ac26e4d0ea5c250bb6088f15fdef40b
Thomas Paviot <tpaviot@gmail.com>
2020-04-07 15:00:49 +0200

    Added py.typed to the Core package, new PkgBase directory that contains files to be installed to the OCC/Core directory

--- cmake/__init__.py
+++
@@ -1,9 +0,0 @@
-PYTHONOCC_VERSION_MAJOR = 7
-PYTHONOCC_VERSION_MINOR = 4
-PYTHONOCC_VERSION_PATCH = 1
-
-#  Empty for official releases, set to -dev, -rc1, etc for development releases
-PYTHONOCC_VERSION_DEVEL = '-dev'
-
-VERSION = "%s.%s.%s%s" % (PYTHONOCC_VERSION_MAJOR, PYTHONOCC_VERSION_MINOR,
-                          PYTHONOCC_VERSION_PATCH, PYTHONOCC_VERSION_DEVEL)
