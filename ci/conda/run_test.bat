3421a803db1c8fc5113a9acc79377e50bdc1257b
Thomas Paviot <tpaviot@gmail.com>
2020-04-17 14:11:24 +0200

    Added occ_classic_bottle mypy test

--- ci/conda/run_test.bat
+++ ci/conda/run_test.bat
@@ -1,2 +1,3 @@
 cd ..\work\test
 python core_wrapper_features_unittest.py
+mypy test_mypy_classic_occ_bottle.py
--- ci/conda/run_test.sh
+++ ci/conda/run_test.sh
@@ -2,5 +2,4 @@
 cd ../work/test
 python run_tests.py
 python core_webgl_unittest.py
-mypy core_wrapper_features_unittest.py
-mypy core_ocaf_unittest.py
+mypy test_mypy_classic_occ_bottle.py
