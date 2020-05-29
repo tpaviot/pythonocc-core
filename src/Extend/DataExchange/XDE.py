890e963733a733514e80b1c7375e4bb355c06cb9
Thomas Paviot <tpaviot@gmail.com>
2020-05-29 11:24:42 +0200

    pylint cleanups

--- src/Extend/DataExchange/XDE.py
+++ src/Extend/DataExchange/XDE.py
@@ -19,22 +19,18 @@
 import os
 import re
 
-from OCC.Core.TopoDS import TopoDS_Shell
 from OCC.Core.BRep import BRep_Builder
-from OCC.Core.XCAFDoc import (XCAFDoc_DocumentTool_ShapeTool,
-                              XCAFDoc_DocumentTool_ColorTool)
-from OCC.Core.TDocStd import TDocStd_Document
-from OCC.Core.TCollection import TCollection_ExtendedString
-from OCC.Core.XCAFDoc import (XCAFDoc_DocumentTool_ShapeTool,
-                              XCAFDoc_DocumentTool_ColorTool,
-                              XCAFDoc_DocumentTool_LayerTool,
-                              XCAFDoc_DocumentTool_MaterialTool)
-
-from OCC.Core.TDF import TDF_LabelSequence, TDF_Label
+from OCC.Core.IFSelect import IFSelect_RetDone
 from OCC.Core.Quantity import Quantity_Color, Quantity_TOC_RGB
 from OCC.Core.STEPCAFControl import STEPCAFControl_Reader
+from OCC.Core.TDocStd import TDocStd_Document
+from OCC.Core.TCollection import TCollection_ExtendedString
+from OCC.Core.TDF import TDF_LabelSequence, TDF_Label
+from OCC.Core.TopoDS import TopoDS_Shell
+from OCC.Core.XCAFDoc import (XCAFDoc_DocumentTool_ShapeTool,
+                              XCAFDoc_DocumentTool_ColorTool)
+
 from OCC.Extend.TopologyUtils import TopologyExplorer, get_type_as_string
-from OCC.Core.IFSelect import IFSelect_RetDone, IFSelect_ItemsByEntity
 
 class DocFromSTEP:
     def __init__(self, stp_filename, doc_name=""):
@@ -75,17 +71,16 @@ class SceneGrapheFromDoc:
         self._log = log
 
         self._get_shapes()
-    
+
     def get_scene(self):
         return self._scene
-    
+
     def get_internalFaceEntries(self):
         return self._facesInSubshapes
 
     def _print_log(self, message):  # TODO: replace with the logging module functions
         if self._log:
             print(message)
-        return
 
     def _get_shapes(self):
         labels = TDF_LabelSequence()
@@ -116,15 +111,15 @@ class SceneGrapheFromDoc:
             self._shape_tool.GetComponents(lab, l_c)
             for i in range(l_c.Length()):
                 label = l_c.Value(i + 1)
-                #print("Group Name DEF :", name, labelString)    
+                #print("Group Name DEF :", name, labelString)
                 if self._shape_tool.IsReference(label):
-                    self._print_log("########  component label :"+ self._unescapeStep(label.GetLabelName()) )
+                    self._print_log("########  component label :" + self._unescapeStep(label.GetLabelName()))
                     loc = self._shape_tool.GetLocation(label)
                     #print(" Transform  loc DEF          :", loc.HashCode(100))
                     label_reference = TDF_Label()
                     self._shape_tool.GetReferredShape(label, label_reference)
                     reference_name = self._unescapeStep(label_reference.GetLabelName())
-                    self._print_log("########  Transform USE to DEF ==> referenced label : "+ reference_name )
+                    self._print_log("########  Transform USE to DEF ==> referenced label : "+ reference_name)
                     trafo = {'node' : 'Transform',
                              'DEF' : label.EntryDumpToString(),
                              'name' : reference_name + '-trafo',
@@ -163,14 +158,14 @@ class SceneGrapheFromDoc:
             l_subss = TDF_LabelSequence()
             self._shape_tool.GetSubShapes(lab, l_subss)
             subcolorsUniform = True
-            
+
             #always use Transform type for proper USE
             node = {'node': 'Transform',
                     'DEF': labelString,
                     'name': name,
                     'children': []
                     }
-            
+
             shapenode = {'node' : 'Shape',
                          'label' : lab,
                          'shape' : shape,
@@ -180,21 +175,17 @@ class SceneGrapheFromDoc:
                          'color' : (c.Red(), c.Green(), c.Blue()),
                          'colorDEF' : clabelString
                         }
-            
+
             #shapenode is attached below as needed
 
-            if (l_subss.Length() == 0):
-                
+            if l_subss.Length() == 0:
                 node['name'] = node['name'] + '-wrapper'
-                
                 shapenode['name'] = name + '-singleshape'
 
-            if (not labloc.IsIdentity()):
-                
+            if not labloc.IsIdentity():
                 node['transform'] = labloc
-                
-            for i in range(l_subss.Length()):
 
+            for i in range(l_subss.Length()):
                 lab_subs = l_subss.Value(i+1)
                 shape_sub = self._shape_tool.GetShape(lab_subs)
                 shape_type = get_type_as_string(shape_sub)
@@ -208,7 +199,7 @@ class SceneGrapheFromDoc:
                 c = self._set_color(lab_subs, shape_sub)
                 clabel = self._color_tool.FindColor(c)
                 clabelString = clabel.EntryDumpToString()
-                n = c.Name(c.Red(), c.Green(), c.Blue())
+                #n = c.Name(c.Red(), c.Green(), c.Blue())
                 #print('    solidshape color RGB: ', c.Red(), c.Green(), c.Blue(), n)
                 node_name = self._unescapeStep(lab_subs.GetLabelName())
                 def_name = lab_subs.EntryDumpToString()
@@ -216,38 +207,38 @@ class SceneGrapheFromDoc:
                 #print("    subshape Transform: ", subloc.HashCode(100))
                 #default subshape
                 subshapenode = {'node': 'SubShape',
-                             'label': lab_subs,
-                             'shape': shape_sub,
-                             'shapeType': shape_type,
-                             'DEF': def_name,
-                             'name': node_name + '-subshape',
-                             'colorString': f"{c.Red()} {c.Green()} {c.Blue()}",
-                             'color': (c.Red(), c.Green(), c.Blue()),
-                             'colorDEF': clabelString,
-                             'trafo': subloc
-                            }
+                                'label': lab_subs,
+                                'shape': shape_sub,
+                                'shapeType': shape_type,
+                                'DEF': def_name,
+                                'name': node_name + '-subshape',
+                                'colorString': f"{c.Red()} {c.Green()} {c.Blue()}",
+                                'color': (c.Red(), c.Green(), c.Blue()),
+                                'colorDEF': clabelString,
+                                'trafo': subloc
+                                }
 
                 ### look for face colors
                 expl = TopologyExplorer(shape_sub)
                 solidfaces = list(expl.faces()) # works for all types
                 #hasMultiColor = False
-                if len(solidfaces) > 0:
+                if solidfaces:
                     colorFaceLists = {}
                     colorColors = {}
                     #print ("shapetype of solidface: ", get_type_as_string(solidfaces[0]))
                     facelabel = TDF_Label()
-                    for i in range(0, len(solidfaces)):
-                        found = self._shape_tool.FindSubShape(lab, solidfaces[i], facelabel)
+                    for solidface in solidfaces:
+                        found = self._shape_tool.FindSubShape(lab, solidface, facelabel)
                         if found:
                             self._facesInSubshapes.add(facelabel.EntryDumpToString())
-                            c = self._set_color(facelabel, solidfaces[i])
+                            c = self._set_color(facelabel, solidface)
                             clabel = self._color_tool.FindColor(c)
                             clabelString = clabel.EntryDumpToString()
                             if clabelString not in colorColors:
                                 colorFaceLists[clabelString] = []
-                            colorFaceLists[clabelString].append(solidfaces[i]) # collect face
+                            colorFaceLists[clabelString].append(solidface) # collect face
                             colorColors[clabelString] = c # collect color
-                    
+
                     # override default color, if only one color, is last color
                     clabel = self._color_tool.FindColor(c)
                     clabelString = clabel.EntryDumpToString()
@@ -261,15 +252,14 @@ class SceneGrapheFromDoc:
 
                     # if more colors, make group with a shell per color (or compounds ?)
                     if len(list(colorFaceLists)) > 1:
-                        
                         subshapenode = {'node' : 'Group',
-                                     'label' : lab_subs,
-                                     'shape' : shape_sub,
-                                     'shapeType' : shape_type,
-                                     'DEF' : def_name,
-                                     'name' : node_name+'-colorshells',
-                                     'children' : []
-                                    }
+                                        'label' : lab_subs,
+                                        'shape' : shape_sub,
+                                        'shapeType' : shape_type,
+                                        'DEF' : def_name,
+                                        'name' : node_name+'-colorshells',
+                                        'children' : []
+                                        }
                         f = 0
                         for entry in iter(colorFaceLists):
                             #make new shell with faces of single color
@@ -277,15 +267,15 @@ class SceneGrapheFromDoc:
                             subshell = TopoDS_Shell() #use compound ?
                             builder.MakeShell(subshell)
                             faces = colorFaceLists[entry]
-                            for i in range(0, len(faces)): # add faces
+                            for face in faces: # add faces
                                 #print(entry,i,colorLists[entry][i])
-                                builder.Add(subshell, faces[i])
+                                builder.Add(subshell, face)
                             #shape_type = get_type_as_string(subshell)
                             shape_type = "Shell"
                             c = colorColors[entry]
                             clabel = self._color_tool.FindColor(c)
                             clabelString = clabel.EntryDumpToString()
-                            if (clabelString != shapenode['colorDEF']):
+                            if clabelString != shapenode['colorDEF']:
                                 subcolorsUniform = False
                             shellnode = {'node' : 'SubShape',
                                          'label' : lab_subs,
@@ -306,7 +296,7 @@ class SceneGrapheFromDoc:
             #//end subshapes
             # only attach container shape if all face colors have the same color, required for buggy suspension
             # TODO: look for less heuristics
-            if (subcolorsUniform):
+            if subcolorsUniform:
                 node['children'].append(shapenode)
         parent.append(node)
 
@@ -335,20 +325,20 @@ class SceneGrapheFromDoc:
         return c
 
     def _unescapeStep(self, name):
-        
+
         def _toUnicode(match):
             return chr(int(match.group(1), 16))
 
         reg1 = re.compile(r'\\X\\(..)')
         reg2 = re.compile(r'\\X2\\(....)\\X0\\')
         reg3 = re.compile(r'\\X4\\(........)\\X0\\')
-        
+
         return reg3.sub(_toUnicode, reg2.sub(_toUnicode, reg1.sub(_toUnicode, name)))
 
 
 if __name__ == "__main__":
     # test with the as1_pe.stp file
-    stp_filename = os.path.join('..', '..', '..', 'test', 'test_io', 'as1_pe_203.stp')
-    doc_exp = DocFromSTEP(stp_filename)
-    doc = doc_exp.get_doc()
-    SceneGrapheFromDoc(doc, log=True)
+    step_file = os.path.join('..', '..', '..', 'test', 'test_io', 'as1_pe_203.stp')
+    doc_exp = DocFromSTEP(step_file)
+    document = doc_exp.get_doc()
+    SceneGrapheFromDoc(document, log=True)
