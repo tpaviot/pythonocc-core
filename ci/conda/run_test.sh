a7cf6814ad8990c3a43b3a082140bf650b527de9
Thomas Paviot <tpaviot@gmail.com>
2020-06-17 14:57:20 +0200

    xvfb tst back on Linux

--- ci/conda/run_test.sh
+++ ci/conda/run_test.sh
@@ -2,3 +2,12 @@
 cd ../work/test
 python run_tests.py
 mypy test_mypy_classic_occ_bottle.py
+
+if [ $(uname) == Linux ]; then
+    # start xvfb
+    export DISPLAY=:99.0
+    sh -e /etc/init.d/xvfb start
+    sleep 3 # give xvfb some time to start
+    # then run pyqt5 GUI test
+    python core_display_pyqt5_unittest.py
+fi
