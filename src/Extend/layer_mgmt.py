690f6c1fa9324da0e943c25acbc5926f556718cc
Tanneguy <tanneguydv@gmail.com>
2021-11-04 15:51:38 +0100

    Update layer_mgmt.py

--- src/Extend/layer_mgmt.py
+++ src/Extend/layer_mgmt.py
@@ -1,3 +1,20 @@
+##Copyright 2021 Tanneguy de Villemagne (tanneguydv@gmail.com)
+##
+##This file is part of pythonOCC.
+##
+##pythonOCC is free software: you can redistribute it and/or modify
+##it under the terms of the GNU Lesser General Public License as published by
+##the Free Software Foundation, either version 3 of the License, or
+##(at your option) any later version.
+##
+##pythonOCC is distributed in the hope that it will be useful,
+##but WITHOUT ANY WARRANTY; without even the implied warranty of
+##MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+##GNU Lesser General Public License for more details.
+##
+##You should have received a copy of the GNU Lesser General Public License
+##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
+
 class Layer():
 
     def __init__(self):
