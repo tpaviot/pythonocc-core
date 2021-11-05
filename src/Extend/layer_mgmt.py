4669ac9dbd63b6444ceabf43ce87092e46cc5112
Tanneguy <tanneguydv@gmail.com>
2021-11-05 15:57:57 +0100

    Update layer_mgmt.py
    delete create method, add get_shapes and join

--- src/Extend/layer_mgmt.py
+++ src/Extend/layer_mgmt.py
@@ -15,33 +15,55 @@
 ##You should have received a copy of the GNU Lesser General Public License
 ##along with pythonOCC.  If not, see <http://www.gnu.org/licenses/>.
 
-class Layer():
+class Layer:
 
-    def __init__(self):
+    def __init__(self, from_display, shape=None, color=0):
+        """
+        :param from_display: the display from the main code
+        :param shape: TopoDS_Shape
+        :param color: Quantity color
+        """
         self.clear()
-
-    def create(self, shape, color=0):
         self.color = color
-        self.to_display = display.DisplayShape(shape, color=self.color)[0]
-        self.count += 1
-        self.list_to_display.append(self.to_display)
-        display.Context.Erase(self.to_display, False)
-
-    def show(self):
-        for shape in self.list_to_display:
-            display.Context.Display(shape, True)
-
-    def hide(self):
-        for shape in self.list_to_display:
-            display.Context.Erase(shape, False)
+        self.display = from_display
+        if shape is not None:
+            self.add(shape)
 
     def add(self, shape):
         self.to_display = {self.count}
-        self.to_display = display.DisplayShape(shape, color=self.color)[0]
+        self.to_display = self.display.DisplayShape(shape, color=self.color)[0]
         self.list_to_display.append(self.to_display)
-        display.Context.Erase(self.to_display, False)
+        self.shapes.append(shape)
+        self.count += 1
+        self.display.Context.Erase(self.to_display, False)
 
     def clear(self):
         self.list_to_display = []
         self.count = 0
         self.to_display = {self.count}
+        self.shapes = []
+
+    def get_shapes(self):
+        """
+        :return: TopoDS_Shape
+        """
+        return self.shapes
+
+    def hide(self):
+        for shape in self.list_to_display:
+            self.display.Context.Erase(shape, False)
+
+    def join(self, layer, clear=False):
+        """
+        :param layer: name of the layer to join
+        :param clear: bool to clear the layer to join
+        :return: None
+        """
+        for shape in layer.get_shapes():
+            self.add(shape)
+        if clear is True:
+            layer.clear()
+
+    def show(self):
+        for shape in self.list_to_display:
+            self.display.Context.Display(shape, True)
