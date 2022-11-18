c436f5c69737175887393908cb96ff5e4a03c992
tpaviot <tpaviot@gmail.com>
2022-11-18 05:49:01 +0100

    Bump version number to 7.7.0

--- src/PkgBase/__init__.py
+++ src/PkgBase/__init__.py
@@ -1,6 +1,6 @@
 PYTHONOCC_VERSION_MAJOR = 7
-PYTHONOCC_VERSION_MINOR = 6
-PYTHONOCC_VERSION_PATCH = 3
+PYTHONOCC_VERSION_MINOR = 7
+PYTHONOCC_VERSION_PATCH = 0
 
 #  Empty for official releases, set to -dev, -rc1, etc for development releases
 PYTHONOCC_VERSION_DEVEL = ''
