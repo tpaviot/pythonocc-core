584bd55651f73dbca637b5a01226722a813cedb3
tpaviot <tpaviot@gmail.com>
2023-04-13 16:40:05 +0200

    remove xvfb tests from conda build

--- ci/conda/run_test.sh
+++ ci/conda/run_test.sh
@@ -2,10 +2,3 @@
 cd ../work/test
 python run_tests.py
 mypy test_mypy_classic_occ_bottle.py
-
-if [ $(uname) == Linux ]; then
-    # start xvfb
-    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x24' python core_display_pyqt5_unittest.py
-    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x24' python core_display_pyside2_unittest.py
-    xvfb-run --auto-servernum --server-args='-screen 0, 1024x768x24' python core_display_wx_unittest.py
-fi
