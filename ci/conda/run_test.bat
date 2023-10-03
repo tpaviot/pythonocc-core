965faae610c84e806732cfa420b5c6625637e52b
Thomas Paviot <tpaviot@gmail.com>
2023-10-03 06:59:00 +0200

    Add PySide6 test on windows

--- ci/conda/run_test.bat
+++ ci/conda/run_test.bat
@@ -3,4 +3,5 @@ python run_tests.py
 mypy test_mypy_classic_occ_bottle.py
 python core_display_pyqt5_unittest.py
 python core_display_pyside2_unittest.py
+python core_display_pyside6_unittest.py
 python core_display_wx_unittest.py
