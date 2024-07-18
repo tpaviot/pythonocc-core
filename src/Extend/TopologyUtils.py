2fd81e2094b202295d66ebbff8ad0174a5855ff1
Rafael Senties Martinelli <rafael@senties-martinelli.com>
2024-07-18 11:07:32 +0200

    Fix TopTools_ListIteratorOfListOfShape import
    TopTools_ListIteratorOfListOfShape must be replaced by TopTools_ListOfListOfShape

--- src/Extend/TopologyUtils.py
+++ src/Extend/TopologyUtils.py
@@ -38,7 +38,7 @@ from OCC.Core.TopAbs import (
 )
 from OCC.Core.TopExp import TopExp_Explorer, topexp
 from OCC.Core.TopTools import (
-    TopTools_ListIteratorOfListOfShape,
+    TopTools_ListOfListOfShape,
     TopTools_IndexedDataMapOfShapeListOfShape,
 )
 from OCC.Core.TopoDS import (
@@ -332,7 +332,7 @@ class TopologyExplorer:
         if results.Size() == 0:
             yield None
 
-        topology_iterator = TopTools_ListIteratorOfListOfShape(results)
+        topology_iterator = TopTools_ListOfListOfShape(results)
         while topology_iterator.More():
             topo_entity = self.topology_factory[topology_type_2](
                 topology_iterator.Value()
@@ -380,7 +380,7 @@ class TopologyExplorer:
         results = _map.FindFromKey(topological_entity)
         if results.Size() == 0:
             return None
-        topology_iterator = TopTools_ListIteratorOfListOfShape(results)
+        topology_iterator = TopTools_ListOfListOfShape(results)
         while topology_iterator.More():
             topo_set.add(topology_iterator.Value())
             topology_iterator.Next()
